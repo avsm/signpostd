ff7c4d2b5330fdf5b043e78962f4df45ad6b9fbe
Haris Rotsos <cr409@cam.ac.uk>
2012-03-02 14:30:18 +0000

    Adding a dns server on client data

--- lib/client.ml
+++ lib/client.ml
@@ -30,6 +30,46 @@ let local_ips = ref []
 
 let usage () = eprintf "Usage: %s <node-name> <node-ip> <node-signalling-port>\n%!" Sys.argv.(0); exit 1
 
+let nxdomain =
+  {Dns.Query.rcode=`NXDomain; aa=false; answer=[]; authority=[]; additional=[]}
+
+  (* Figure out the response from a query packet and its question section *)
+let get_response packet q =
+  let open Dns.Packet in
+  let module DQ = Dns.Query in
+  (* Normalise the domain names to lower case *)
+  let qnames = List.map String.lowercase q.q_name in
+  eprintf "Q: %s\n%!" (String.concat " " qnames); 
+  nxdomain 
+(*     return nxdomain *)
+(*
+  let from_trie = Dns.(Query.answer_query q.q_name q.q_type Loader.(state.db.trie)) in
+  match qnames with
+    (* For this strawman, we assume a valid query has form
+     * <dst node>.<src node>.<domain name>
+     *)
+  |dst::src::domain -> begin
+     let domain'=String.concat "." domain in
+     if domain' = our_domain then begin
+       eprintf "src:%s dst:%s dom:%s\n%!" src dst domain';
+       ip_resp ~dst ~src ~domain
+     end else from_trie
+  end
+  |_ -> from_trie
+
+*)
+
+let dnsfn ~src ~dst packet =
+  let open Dns.Packet in
+  match packet.questions with
+  |[] -> eprintf "bad dns query: no questions\n%!"; return None
+  |[q] -> return (Some (get_response packet q))
+  |_ -> eprintf "dns dns query: multiple questions\n%!"; return None
+
+let dns_t () =
+  lwt fd, src = Dns_server.bind_fd ~address:"127.0.0.1" ~port:5354 in
+    Dns_server.listen ~fd ~src ~dnsfn 
+
 let update_server_if_state_has_changed () =
   let ips = Nodes.discover_local_ips () in
   match (ips <> !local_ips) with
@@ -60,6 +100,7 @@ let _ =
   let daemon_t = join 
   [ 
     client_t (); 
-    signal_t ~port:!node_port
+    signal_t ~port:!node_port;
+    dns_t ();
   ] in
   Lwt_main.run daemon_t
