fb3924bc114534257ca5ded6ee6108aef4849408
cr409 <cr409@cl.cam.ac.uk>
2012-04-22 17:23:33 +0100

    [tor] minor fixes in tor server code

--- lib/torConnection.ml
+++ lib/torConnection.ml
@@ -21,52 +21,52 @@ open Printf
 open Int64
 
 
-exception Privoxy_error
+exception Tor_error
 
-let name () = "privoxy"
+let name () = "tor"
 
 (******************************************************
  * connection method
  ******************************************************)
 let connect a b =
   (* Trying to see if connectivity is possible *)
-  eprintf "[proxy] enabling between privoxy on %s \n%!" a;
-  let rpc = (Rpc.create_tactic_request "privoxy" 
+  eprintf "[proxy] enabling between tor on %s \n%!" a;
+  let rpc = (Rpc.create_tactic_request "tor" 
                Rpc.CONNECT "start" []) in
     try
       lwt res = (Nodes.send_blocking a rpc) in 
-      let rpc = (Rpc.create_tactic_request "privoxy" 
+      let rpc = (Rpc.create_tactic_request "tor" 
                    Rpc.CONNECT "forward" []) in
       lwt res = (Nodes.send_blocking a rpc) in 
         return ()
     with exn -> 
-      Printf.printf "[proxy] client fail %s\n%!" a;
-      raise Privoxy_error
+      Printf.printf "[socks] client fail %s\n%!" a;
+      raise Tor_error
         return ()
   
 
 (* ******************************************
- * A tactic to forward all traffic to the privoxy proxy
+ * A tactic to forward all traffic to the tor proxy
  * ******************************************)
 
 let handle_request action method_name arg_list =
   let open Rpc in
   match action with
   | TEST ->
-      eprintf "[privoxy] doesn't support test action\n%!";
-      return(Sp.ResponseError "Privoxy test is not supported yet")
+      eprintf "[socks] doesn't support test action\n%!";
+      return(Sp.ResponseError "Tor test is not supported yet")
   | CONNECT ->
       (try
-         printf "[privoxy] executing connect command\n%!";
+         printf "[socks] executing connect command\n%!";
          lwt ip = Privoxy.Manager.connect method_name arg_list in
             return(Sp.ResponseValue ip)            
        with e -> 
          return (Sp.ResponseError "provxy_connect"))            
   | TEARDOWN ->
-      eprintf "[privoxy] doesn't support teardown action\n%!";
-      return(Sp.ResponseError "Privoxy teardown is not supported yet")
+      eprintf "[socks] doesn't support teardown action\n%!";
+      return(Sp.ResponseError "Tor teardown is not supported yet")
 
 let handle_notification action method_name arg_list =
-  eprintf "Privoxy tactic doesn't handle notifications\n%!";
+  eprintf "Tor tactic doesn't handle notifications\n%!";
   return ()
 
